3ba9e685354e808dab921ea42fed570b468b21a8
leafney <babycoolzx@126.com>
2023-04-29 20:36:57 +0800

    feat: basic logger

--- zap.go
+++ zap.go
@@ -1,1 +1,66 @@
 package rzap
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+type Logger struct {
+	logger *zap.Logger
+}
+
+func New() *Logger {
+	config := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000"),
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	encoder := zapcore.NewJSONEncoder(config)
+
+	writer := zapcore.AddSync(os.Stdout)
+
+	core := zapcore.NewCore(encoder, writer, zap.InfoLevel)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+	return &Logger{
+		logger: logger,
+	}
+}
+
+func (l *Logger) Debug(msg string, fields ...zap.Field) {
+	l.logger.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...zap.Field) {
+	l.logger.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...zap.Field) {
+	l.logger.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...zap.Field) {
+	l.logger.Error(msg, fields...)
+}
+
+func (l *Logger) Fatal(msg string, fields ...zap.Field) {
+	l.logger.Fatal(msg, fields...)
+}
+
+func (l *Logger) Panic(msg string, fields ...zap.Field) {
+	l.logger.Panic(msg, fields...)
+}
+
+func (l *Logger) Sync() error {
+	return l.logger.Sync()
+}
--- zap_test.go
+++ zap_test.go
@@ -1,1 +1,16 @@
 package rzap
+
+import (
+	"go.uber.org/zap"
+	"testing"
+)
+
+func TestNewLogger(t *testing.T) {
+
+	log := New()
+	defer log.Sync()
+
+	log.Info("测试日志输出", zap.String("name", "tom"))
+	log.Debug("调试日志", zap.Int("age", 20))
+	log.Info("可以可以")
+}
